9a119f8af331554f71d912fb209dba519c0f3853
benj-chen <benjchen06@gmail.com>
2020-08-04 11:36:15 -0700

    Complete number.spiral.py (remove *), change 2.knights.py,2.sets.py to two.knights.py,two.sets.py

---
+++ cses/number.spiral.py
@@ -0,0 +1,16 @@
+n=int(input())
+ins=[list(map(int,input().split())) for x in range(n)]
+for z in ins:
+    y,x=z
+    if x==y:
+        print(x**2-x+1)
+        continue
+    else:
+        e=x
+        if x<y:
+            (x,y)=(y,x)
+            e+=1
+        if e%2==0:
+            print(x**2-2*x+1+y)
+        else:
+            print(x**2-y+1)
