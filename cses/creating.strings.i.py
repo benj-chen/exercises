ca0f47869b3e35601ca11f8f83cb6bb91a98d015
benj-chen <benjchen06@gmail.com>
2020-08-05 11:11:52 -0700

    Complete creating.strings.i.py (use recursion)

---
+++ cses/creating.strings.i.py
@@ -0,0 +1,24 @@
+# https://cses.fi/problemset/task/1622
+
+def ans(l):
+    """
+
+    :param l: list
+    :return: all of the unique ways to permute the list items
+    """
+    if len(l)==1: #base case
+        return l
+    res=[]
+    c=sorted(l[:])
+    for x in l:
+        del c[c.index(x)]
+        t=ans(c)
+        for y in t:
+            res.append(x+y)
+        c=sorted(l[:]) #add back the deleted item (restore for the next iteration)
+    return res
+
+e=set(ans(list(input())))
+print(len(e))
+for x in sorted(e):
+    print(x)
