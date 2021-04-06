aca4118e1dfd9b21fb1ba0cdd07510e2de0dd28a
Benjamin Chen <benjchen06@gmail.com>
2021-04-05 17:38:00 -0700

    test commit

---
+++ google/template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define FOR(a) for (int i=0;i<a;i++)
+#define EACH(a,b) for (auto& a: b)
+#define vt vector
+#define pii pair<int,int>
+#define vi vector<int>
+
+
+void solve() {
+    ;
+}
+int main() {
+    int T;
+    cin >> T;
+    FOR(T) {
+        cout << "Case #" << i+1 << ": ";
+        solve();
+    }
+}
--- sandbox.cpp
+++ sandbox.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
-#include <string>
-using namespace std;
-using str = string;
 int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        int A,B,N;
-        cin >> A >> B >> N;
-        B--;
-        str in;
-        
-        while (in!="CORRECT" && N--) {
-            int md= (A+B)/2;
-            cout << md << endl;
-            cin >> in;
-            if (in=="TOO_SMALL") A=md;
-            else B=md;
-        }
-    }
+    int a;
+    std::cin >> a;
+    std::cout << a << std::endl;
 }
