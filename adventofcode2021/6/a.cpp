8f04a8ad3472093d3bba45c2be17926e9d57af30
Benjamin Chen <benjchen06@gmail.com>
2021-12-13 18:21:19 -0800

    day 6 fix

--- adventofcode2021/6/a.cpp
+++ adventofcode2021/6/a.cpp
@@ -8,7 +8,7 @@ int main() {
         cin.ignore(); // comma
         fish[i]++;
     }
-    for (int i=0;i<256;i++)
+    for (int i=0;i<80;i++)
         fish[(i+7)%9]+=fish[i%9];
     long long ct = 0;
     for (long long i: fish) {
